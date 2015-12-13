6fe6aabb28e8a5b698f609e1c1de4bea1d8f5fe8
Keiichi Sato <nanoks@gmail.com>
2015-12-13 17:42:47 -0500

    implement piece moving

--- board.rb
+++ board.rb
@@ -39,6 +39,17 @@ class Pawn < Piece
             end
         end
     end
+    def valid_moves(board)
+
+        valid_moves = []
+        if color == :w 
+        valid_moves = [board[square.row-1][square.col],board[square.row-2][square.col]] 
+        elsif color == :b
+        valid_moves = [board[square.row+1][square.col],board[square.row+2][square.col]]
+        end
+        valid_moves
+    end 
+
 end 
 class Rook < Piece 
     def display 
@@ -126,24 +137,24 @@ class Queen < Piece
     end
 end 
 
-board = Array.new(8).map!{Array.new(8)}
+@board = Array.new(8).map!{Array.new(8)}
 
 #every other row
-board.each_index do |row| 
+@board.each_index do |row| 
     if row % 2 == 0 
-        board[row].each_index do |col|
+        @board[row].each_index do |col|
             if col % 2 == 0 
-                board[row][col] = Square.new(:w, row, col)
+                @board[row][col] = Square.new(:w, row, col)
             else
-                board[row][col] = Square.new(:b, row, col)
+                @board[row][col] = Square.new(:b, row, col)
             end
         end
     else 
-        board[row].each_index do |col|
+        @board[row].each_index do |col|
             if col % 2 == 0 
-                board[row][col] = Square.new(:b, row, col)
+                @board[row][col] = Square.new(:b, row, col)
             else
-                board[row][col] = Square.new(:w, row, col)
+                @board[row][col] = Square.new(:w, row, col)
             end
         end       
     end 
@@ -152,52 +163,66 @@ end
 
 
  
-board[6].each_index do |c|
-    board[6][c].contains = Pawn.new(:w, board[6][c])
+@board[6].each_index do |c|
+    @board[6][c].contains = Pawn.new(:w, @board[6][c])
 end
-board[1].each_index do |c|
-    board[1][c].contains = Pawn.new(:b, board[1][c]) 
+@board[1].each_index do |c|
+    @board[1][c].contains = Pawn.new(:b, @board[1][c]) 
 end
 
-board[7][0].contains = Rook.new(:w, board[7][0])
-board[7][7].contains = Rook.new(:w, board[7][7])
+@board[7][0].contains = Rook.new(:w, @board[7][0])
+@board[7][7].contains = Rook.new(:w, @board[7][7])
 
-board[0][0].contains = Rook.new(:b, board[0][0])
-board[0][7].contains = Rook.new(:b, board[0][7])
+@board[0][0].contains = Rook.new(:b, @board[0][0])
+@board[0][7].contains = Rook.new(:b, @board[0][7])
 
-board[7][1].contains = Knight.new(:w, board[7][1]) 
-board[7][6].contains = Knight.new(:w, board[7][6]) 
+@board[7][1].contains = Knight.new(:w, @board[7][1]) 
+@board[7][6].contains = Knight.new(:w, @board[7][6]) 
 
-board[0][1].contains = Knight.new(:b, board[0][1])  
-board[0][6].contains = Knight.new(:b, board[0][6])  
+@board[0][1].contains = Knight.new(:b, @board[0][1])  
+@board[0][6].contains = Knight.new(:b, @board[0][6])  
 
-board[7][2].contains = Bishop.new(:w, board[7][2]) 
-board[7][5].contains = Bishop.new(:w, board[7][5]) 
+@board[7][2].contains = Bishop.new(:w, @board[7][2]) 
+@board[7][5].contains = Bishop.new(:w, @board[7][5]) 
 
-board[0][2].contains = Bishop.new(:b, board[0][2]) 
-board[0][5].contains = Bishop.new(:b, board[0][5])  
+@board[0][2].contains = Bishop.new(:b, @board[0][2]) 
+@board[0][5].contains = Bishop.new(:b, @board[0][5])  
 
-board[7][3].contains = Queen.new(:w, board[7][3])
-board[0][3].contains = Queen.new(:b, board[0][3])
+@board[7][3].contains = Queen.new(:w, @board[7][3])
+@board[0][3].contains = Queen.new(:b, @board[0][3])
 
-board[7][4].contains = King.new(:w, board[7][4])
-board[0][4].contains = King.new(:b, board[0][4])
-
-
-
-p board 
+@board[7][4].contains = King.new(:w, @board[7][4])
+@board[0][4].contains = King.new(:b, @board[0][4])
 
 
+def valid_square(square)
+    v_sq = []
+    square.contains.valid_moves.each do |r|
+        v_sq << @board[r[0]][r[1]]
+    end 
+    v_sq
+end
+
+
+def move(from, to)
+    if from.contains != :e && from.contains.valid_moves(@board).include?(to)
+        to.contains = from.contains 
+        from.contains.square = to 
+        from.contains = :e 
+        true
+    else
+        false
+    end
+end 
+
+def display
 d = ""
-
 line = " _________________________________________________________________________________________________________"
-
 d << line 
-
-board.each_index do |row|
+@board.each_index do |row|
     for i in 0..5
         d << "\n||"
-        board[row].each do |sq|
+        @board[row].each do |sq|
             if sq.contains == :e 
                 d << sq.display[i]
             else
@@ -208,9 +233,18 @@ board.each_index do |row|
         d << "|"   
     end 
 end 
-
-
 puts d 
+end 
 
+display 
+gets.chomp
+move(@board[6][4],@board[4][4])
+display 
+=begin
+gets.chomp
 
-p board[6][0].contains.is_a?(Pawn)
+move(@board[6][4],@board[5][4])
+display
+
+=end 
+
