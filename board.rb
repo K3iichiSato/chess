0d724f6b530cdce419bf9fe14e5ee7d7d4c45ed7
Keiichi Sato <nanoks@gmail.com>
2015-12-14 17:28:47 -0500

    reorganize

--- board.rb
+++ board.rb
@@ -144,135 +144,142 @@ class Queen < Piece
     end
 end 
 
-@board = Array.new(8).map!{Array.new(8)}
+class Board
+    attr_accessor :board 
+    def initialize 
+    @board = Array.new(8).map!{Array.new(8)}
 
-#every other row
-@board.each_index do |row| 
-    if row % 2 == 0 
-        @board[row].each_index do |col|
-            if col % 2 == 0 
-                @board[row][col] = Square.new(:w, row, col)
-            else
-                @board[row][col] = Square.new(:b, row, col)
+    #every other row
+    @board.each_index do |row| 
+        if row % 2 == 0 
+            @board[row].each_index do |col|
+                if col % 2 == 0 
+                    @board[row][col] = Square.new(:w, row, col)
+                else
+                    @board[row][col] = Square.new(:b, row, col)
+                end
             end
-        end
-    else 
-        @board[row].each_index do |col|
-            if col % 2 == 0 
-                @board[row][col] = Square.new(:b, row, col)
-            else
-                @board[row][col] = Square.new(:w, row, col)
-            end
-        end       
-    end 
-end
-
-
-
- 
-@board[6].each_index do |c|
-    @board[6][c].contains = Pawn.new(:w, @board[6][c])
-end
-@board[1].each_index do |c|
-    @board[1][c].contains = Pawn.new(:b, @board[1][c]) 
-end
-
-@board[7][0].contains = Rook.new(:w, @board[7][0])
-@board[7][7].contains = Rook.new(:w, @board[7][7])
-
-@board[0][0].contains = Rook.new(:b, @board[0][0])
-@board[0][7].contains = Rook.new(:b, @board[0][7])
-
-@board[7][1].contains = Knight.new(:w, @board[7][1]) 
-@board[7][6].contains = Knight.new(:w, @board[7][6]) 
-
-@board[0][1].contains = Knight.new(:b, @board[0][1])  
-@board[0][6].contains = Knight.new(:b, @board[0][6])  
-
-@board[7][2].contains = Bishop.new(:w, @board[7][2]) 
-@board[7][5].contains = Bishop.new(:w, @board[7][5]) 
-
-@board[0][2].contains = Bishop.new(:b, @board[0][2]) 
-@board[0][5].contains = Bishop.new(:b, @board[0][5])  
-
-@board[7][3].contains = Queen.new(:w, @board[7][3])
-@board[0][3].contains = Queen.new(:b, @board[0][3])
-
-@board[7][4].contains = King.new(:w, @board[7][4])
-@board[0][4].contains = King.new(:b, @board[0][4])
-
-
-def valid_square(square)
-    v_sq = []
-    square.contains.valid_moves.each do |r|
-        v_sq << @board[r[0]][r[1]]
-    end 
-    v_sq
-end
-
-
-def move(from, to)
-    piece = from.contains 
-    if piece != :e && piece.valid_moves(@board).include?(to)
-        to.contains = piece 
-        piece.square = to 
-        from.contains = :e 
-        piece.move += 1 
-        true
-    else
-        false
+        else 
+            @board[row].each_index do |col|
+                if col % 2 == 0 
+                    @board[row][col] = Square.new(:b, row, col)
+                else
+                    @board[row][col] = Square.new(:w, row, col)
+                end
+            end       
+        end 
     end
-end 
 
-def display
-d = ""
-letters= "\n        A            B            C            D            E            F            G            H"
-number = 8 
-d << letters 
-line = "\n _________________________________________________________________________________________________________"
-d << line 
-@board.each_index do |row|
-    for i in 0..5
-        d << "\n"
-        if i == 3 
-            d << number.to_s 
-            number -= 1
+
+
+     
+    @board[6].each_index do |c|
+        @board[6][c].contains = Pawn.new(:w, @board[6][c])
+    end
+    @board[1].each_index do |c|
+        @board[1][c].contains = Pawn.new(:b, @board[1][c]) 
+    end
+
+    @board[7][0].contains = Rook.new(:w, @board[7][0])
+    @board[7][7].contains = Rook.new(:w, @board[7][7])
+
+    @board[0][0].contains = Rook.new(:b, @board[0][0])
+    @board[0][7].contains = Rook.new(:b, @board[0][7])
+
+    @board[7][1].contains = Knight.new(:w, @board[7][1]) 
+    @board[7][6].contains = Knight.new(:w, @board[7][6]) 
+
+    @board[0][1].contains = Knight.new(:b, @board[0][1])  
+    @board[0][6].contains = Knight.new(:b, @board[0][6])  
+
+    @board[7][2].contains = Bishop.new(:w, @board[7][2]) 
+    @board[7][5].contains = Bishop.new(:w, @board[7][5]) 
+
+    @board[0][2].contains = Bishop.new(:b, @board[0][2]) 
+    @board[0][5].contains = Bishop.new(:b, @board[0][5])  
+
+    @board[7][3].contains = Queen.new(:w, @board[7][3])
+    @board[0][3].contains = Queen.new(:b, @board[0][3])
+
+    @board[7][4].contains = King.new(:w, @board[7][4])
+    @board[0][4].contains = King.new(:b, @board[0][4])
+    end 
+
+
+    def move(from, to)
+        piece = from.contains 
+        if piece != :e && piece.valid_moves(@board).include?(to)
+            to.contains = piece 
+            piece.square = to 
+            from.contains = :e 
+            piece.move += 1 
+            true
         else
-            d << " "
+            false
         end
-        d << "||"
-        @board[row].each do |sq|
-            if sq.contains == :e 
-                d << sq.display[i]
+    end 
+
+    def display
+    d = ""
+    letters= "\n        A            B            C            D            E            F            G            H"
+    number = 8 
+    d << letters 
+    line = "\n _________________________________________________________________________________________________________"
+    d << line 
+    @board.each_index do |row|
+        for i in 0..5
+            d << "\n"
+            if i == 3 
+                d << number.to_s 
+                number -= 1
             else
-                d << sq.contains.display[i]
-            end 
-            d << "|"
+                d << " "
+            end
+            d << "||"
+            @board[row].each do |sq|
+                if sq.contains == :e 
+                    d << sq.display[i]
+                else
+                    d << sq.contains.display[i]
+                end 
+                d << "|"
+            end
+            d << "|"   
+            d << (number+1).to_s if i == 3 
+        end 
+    end 
+    d << letters
+    puts d 
+    end 
+end
+
+class Game 
+    attr_accessor :bo
+    def initialize
+    @bo = Board.new
+    end
+
+    def key(letter)
+        list = ('a'..'h').to_a
+        list.find_index(letter)
+    end
+    def row (row)
+        -(row.to_i-8)
+    end
+
+    def input (i = gets.chomp)
+      
+        bo.move(bo.board[row(i[1])][key(i[0])], bo.board[row(i[3])][key(i[2])])
+    end
+
+    def play 
+        bo.display 
+        loop do
+        p input 
+        bo.display
         end
-        d << "|"   
-        d << (number+1).to_s if i == 3 
     end 
 end 
-d << letters
-puts d 
-end 
-def key(letter)
-    list = ('a'..'h').to_a
-    list.find_index(letter)
-end
-def row (row)
-    -(row.to_i-8)
-end
-
-def input (i = gets.chomp)
-  
-    move(@board[row(i[1])][key(i[0])], @board[row(i[3])][key(i[2])])
-end
-display 
-loop do
-input 
-display
-end
 =begin
 gets.chomp
 
